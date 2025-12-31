3213eab578d89b5c72893e756d41eb1eb6fefe3c
Keshav0018 <keshavchandak018@gmail.com>
2026-01-01 00:26:48 +0530

    Added proper variables

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:test1234%21@localhost:5431/TodoApplicationDatabse"
+    "postgresql://todo_db_0i7e_user:zwxfiUBnXFCHfbhZO7CRESX4el81IT9k@dpg-d5an2amr433s738gr0og-a/todo_db_0i7e"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
